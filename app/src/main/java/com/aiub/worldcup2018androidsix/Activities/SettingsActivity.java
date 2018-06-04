72db7c3e4720a7274833f4795cd1fe390ee4a68f
shagufahannan <39925396+shagufahannan@users.noreply.github.com>
2018-06-05 01:43:06 +0600

    Initial commit

---
+++ app/src/main/java/com/aiub/worldcup2018androidsix/Activities/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.aiub.worldcup2018androidsix.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aiub.worldcup2018androidsix.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
